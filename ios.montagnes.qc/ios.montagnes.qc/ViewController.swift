953828fce46efd4a6d0e670db4902841e295efc4
Philippe Landry <philippe.landry.montreal@gmail.com>
2020-08-31 16:25:31 -0400

    bof

--- ios.montagnes.qc/ios.montagnes.qc/ViewController.swift
+++ ios.montagnes.qc/ios.montagnes.qc/ViewController.swift
@@ -7,12 +7,41 @@
 //
 
 import UIKit
+import Alamofire
+
+struct Film: Decodable {
+  let id: Int
+  let title: String
+  let openingCrawl: String
+  let director: String
+  let producer: String
+  let releaseDate: String
+  let starships: [String]
+  
+  enum CodingKeys: String, CodingKey {
+    case id = "episode_id"
+    case title
+    case openingCrawl = "opening_crawl"
+    case director
+    case producer
+    case releaseDate = "release_date"
+    case starships
+  }
+}
+
 
 class ViewController: UIViewController {
-
+    func fetchFilms() {
+      // 1
+      let request = AF.request("https://swapi.dev/api/films")
+      // 2
+      request.responseJSON { (data) in
+        print(data)
+      }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        fetchFilms()
     }
 
 
